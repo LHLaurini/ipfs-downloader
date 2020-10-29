a2329f57f85a5d3acef48db71a558ef9184db01e
Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>
2020-10-29 00:27:14 -0300

    Use tinyify to reduce bundle.js size

--- bundle.js
+++ bundle.js
@@ -1,9 +1,11 @@
 var browserify = require('browserify');
 var tsify = require('tsify');
+var tinyify = require('tinyify');
 
 browserify()
     .add('src/index.ts')
     .plugin(tsify, { noImplicitAny: true })
+    .plugin(tinyify)
     .bundle()
     .on('error', function (error) { console.error(error.toString()); })
     .pipe(process.stdout);
