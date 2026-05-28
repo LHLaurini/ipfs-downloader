e9e1db589b3eb0b4324720f65a3e0f28171288ac
Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>
2026-05-29 01:07:40 +0200

    Update to use Helia, plus other changes
    - Switch from browserify to esbuild
    - Slightly reword README
    - Other minor changes

--- README.md
+++ README.md
@@ -1,32 +1,29 @@
 # ipfs-downloader
 
-ipfs-downloader is a simple script which allows one to download files from the IPFS directly from the browser without using gateways. It creates an IPFS node when the download button is first clicked.
+ipfs-downloader is an example showing how to download files from IPFS directly from the browser without using gateways. It creates an IPFS node once the download button is first clicked.
 
 ## Building
 
 ``` sh
 npm install
-npm run bundle
+npm run build
 ```
 
 ## Usage
 
-1. Add the generated `bundle.js` script and the `styles.css` stylesheet to your page;
+1. Add the generated `bundle.js` and `styles.css` to your page;
 2. For each IPFS file, add:
 
 ``` html
-<a class="ipfs-downloader" data-cid="/ipfs/CID" download="filename.ext">Download label</a>
+<a class="ipfs-downloader" data-path="/ipfs/CID/filename" download="filename.ext">Download label</a>
 ```
 
 See the [public/index.html](public/index.html) example or [try it live](https://ipfs.io/ipfs/QmPNSLu15AaMbxQRb5pe7LjxzyiHd1Xiz44xZoRcUxWxZF) (from IPFS).
 
 ## To-do
 
-* High priority:
-    * Allow downloading directories (zipped);
-    * Don't store file in RAM (should allow downloading larger files).
-* Medium priority:
-    * Customization;
-    * Show details (size, rate, status, ...).
-* Low priority:
-    * Use running node, if present.
+* Allow downloading directories (zipped);
+* Don't store file in RAM (should allow downloading larger files).
+* Customization;
+* Show details (size, rate, status, ...).
+* Use running node, if present.
--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,65 @@
 
-import IPFS from "ipfs";
-import { PromiseValue } from "type-fest";
+import { createHelia } from "helia";
+import { UnixFS, unixfs } from "@helia/unixfs";
+import { CID } from "multiformats/cid";
 
-// Doesn't support import, it seems
-const uint8ArrayConcat = require("uint8arrays/concat");
-
-type Ipfs = PromiseValue<ReturnType<typeof IPFS.create>>;
-
-const OPTIONS = {};
-
-function createDownloadUrl(data: Uint8Array) {
+function createDownloadUrl(data: Uint8Array<ArrayBuffer>) {
     const file = new Blob([data], { type: 'application/octet-binary' });
     return URL.createObjectURL(file);
 }
 
-function downloadIpfsFile(ipfs: Ipfs, cid: string, onProgress: (downloaded: number, total: number) => void) {
-    return new Promise(async function (resolve: (arg: Uint8Array) => void, reject: () => void) {
-        let data: Uint8Array[] = [];
-        let downloaded = 0;
-        let total: number;
-
-        for await (const file of ipfs.get(cid)) {
-            if (downloaded != 0) {
-                // shouldn't get to this point
-                console.error("multiple files");
-                reject();
-                return;
-            }
-
-            if (file.type == "file") {
-                if (file.content != undefined) {
-                    total = file.size;
-
-                    for await (const chunk of file.content) {
-                        data.push(chunk);
-                        downloaded += chunk.length;
-                        onProgress(downloaded, total);
-                    }
-                }
-            } else {
-                console.error("node is not a file");
-                reject();
-                return;
-            }
-        }
-
-        resolve(uint8ArrayConcat(data));
-    });
-}
-
-async function initIpfs() {
-    if (initIpfs.ipfs === undefined) {
-        initIpfs.ipfs = await IPFS.create(OPTIONS);
-        return initIpfs.ipfs;
+async function downloadIpfsFile(unixfs: UnixFS, cid: string, onProgress: (downloaded: number, total: number) => void) {
+    // These functions accept both a CID or a path as a string, the type annotations are incorrect
+    let stat = await unixfs.stat(cid as unknown as CID);
+    if (stat.type != "file") {
+        throw Error("node is not a file");
     }
-    else {
-        return initIpfs.ipfs;
+    
+    let size = Number(stat.size);
+    let data = new Uint8Array(size);
+    
+    let downloaded = 0;
+    for await (const chunk of unixfs.cat(cid as unknown as CID)) {
+        data.set(chunk, downloaded);
+        downloaded += chunk.length;
+        onProgress(downloaded, size);
     }
+
+    return data;
 }
 
-namespace initIpfs {
-    export let ipfs: Ipfs | undefined;
+async function initHelia() {
+    if (initHelia.unixfs === undefined) {
+        initHelia.unixfs = unixfs(await createHelia());
+    }
+    return initHelia.unixfs;
 }
 
-function error(self: HTMLAnchorElement) {
-    self.innerText = "Failed";
-    self.style.setProperty("--progress", "1");
-    self.style.setProperty("--progress-color", "#fa0000");
+namespace initHelia {
+    export let unixfs: UnixFS | undefined;
 }
 
-function download(self: HTMLAnchorElement, cid: string) {
-    initIpfs()
-        .then(function (ipfs: Ipfs) {
-            downloadIpfsFile(ipfs, cid, function (downloaded: number, total: number) {
-                self.style.setProperty("--progress", (downloaded / total).toString());
-            })
-                .then(function (data: Uint8Array) {
-                    self.innerText = "Downloaded";
-                    self.onclick = null;
-                    self.href = createDownloadUrl(data);
-                    self.click();
-                })
-                .catch(function (err) {
-                    console.error(err);
-                    error(self);
-                });
+function setError(element: HTMLAnchorElement) {
+    element.innerText = "Failed";
+    element.style.setProperty("--progress", "1");
+    element.style.setProperty("--progress-color", "#fa0000");
+}
+
+async function download(element: HTMLAnchorElement, cid: string) {
+    try {
+        let unixfs = await initHelia();
+        let data = await downloadIpfsFile(unixfs, cid, function (downloaded: number, total: number) {
+            element.style.setProperty("--progress", (downloaded / total).toString());
         })
-        .catch(function (err) {
-            console.error(err);
-            error(self);
-        })
+
+        element.innerText = "Downloaded";
+        element.onclick = null;
+        element.href = createDownloadUrl(data);
+        element.click();
+    } catch (err) {
+        console.error(err);
+        setError(element);
+    }
 }
 
 window.onload = function () {
@@ -98,13 +68,10 @@ window.onload = function () {
         let cid = downloader.getAttribute("data-cid");
 
         if (downloader instanceof HTMLAnchorElement && cid != null) {
-            let downloader_ = downloader;
-            let cid_ = cid;
-
             downloader.onclick = function () {
-                downloader_.onclick = null;
-                downloader_.innerText = "Downloading...";
-                download(downloader_, cid_);
+                downloader.onclick = null;
+                downloader.innerText = "Downloading...";
+                download(downloader, cid);
             };
         }
     }
